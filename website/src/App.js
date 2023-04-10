704ea4b6f218cbed64936a0508c17535489d3df5
Alan Chang <alan@alanchang.xyz>
2023-04-10 01:11:29 -0700

    [April 2023] Add redirects for socials, fix mobile scrolling further

--- website/src/App.js
+++ website/src/App.js
@@ -12,17 +12,19 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container">
-        <div className="content-wrap">
-          {/* <Nav /> */}
-          <AnimatePresence mode="wait">
-            <Routes key={location.pathname} location={location}>
-              <Route path="/" element={<Home />} />
-              <Route path="*" element={<Redirect url="/" />} />
-            </Routes>
-          </AnimatePresence>
-        </div>
-      </div>
+      {/* <Nav /> */}
+      <AnimatePresence mode="wait">
+        <Routes key={location.pathname} location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/email" element={<Redirect url="mailto:alanc16@uci.edu" />} />
+          <Route path="/github" element={<Redirect url="https://github.com/alanchangxyz" />} />
+          <Route path="/linkedin" element={<Redirect url="https://linkedin.com/in/acxyz" />} />
+          <Route path="/the-county" element={<Redirect url="https://www.notion.so/acxyz/the-county-9c590d5f68b04125bb671fc2fd51d9fc" />} />
+          <Route path="/venmo" element={<Redirect url="https://venmo.com/alanchangxyz" />} />
+          <Route path="/venmo-app" element={<Redirect url="venmo://paycharge?txn=pay&recipients=alanchangxyz" />} />
+          <Route path="*" element={<Redirect url="/" />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
