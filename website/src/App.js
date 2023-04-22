348785b24e812595638963381f4f9cf91f44579a
Alan Chang <alan@alanchang.xyz>
2023-04-21 22:37:48 -0700

    [April 2023] Add netwics links for quick access to slides/demo

--- website/src/App.js
+++ website/src/App.js
@@ -6,7 +6,7 @@ import { AnimatePresence } from "framer-motion";
 import Redirect from "./app/components/Redirect/Redirect";
 import Home from "./app/views/Home/Home";
 
-function App() {
+const App = () => {
   ReactGA.initialize("G-H3X1SVSLE4");
   const location = useLocation();
 
@@ -22,11 +22,13 @@ function App() {
           <Route path="/the-county" element={<Redirect url="https://www.notion.so/acxyz/the-county-9c590d5f68b04125bb671fc2fd51d9fc" />} />
           <Route path="/venmo" element={<Redirect url="venmo://paycharge?txn=pay&recipients=alanchangxyz" />} />
           <Route path="/venmo-p" element={<Redirect url="https://venmo.com/alanchangxyz" />} />
+          <Route path="/netwics-slides" element={<Redirect url="https://docs.google.com/presentation/d/1t65EzhxGAYjgDQDcdoa9pvL2jk5JMLLUkUAQfLCoOTQ/edit?pli=1#slide=id.g210cc2df18a_0_322" />} />
+          <Route path="/netwics-demo" element={<Redirect url="https://codesandbox.io/s/netwics-workshop-s1ef3m?file=/src/App.js" />} />
           <Route path="*" element={<Redirect url="/" />} />
         </Routes>
       </AnimatePresence>
     </div>
   );
-}
+};
 
 export default App;
--- website/src/app/components/ProjectCard/ProjectCard.jsx
+++ website/src/app/components/ProjectCard/ProjectCard.jsx
@@ -9,48 +9,46 @@ import {
 } from "../../../util/animations";
 import styles from "./ProjectCard.module.css";
 
-function ProjectCard({
+const ProjectCard = ({
   name, subhead, color, description,
-}) {
-  return (
-    <motion.div
-      className={styles.projectCard}
-      variants={animationConfigs.rotate(15, -15)}
-      transition={transitionConfigs.spring(1.25, 0, 0.2)}
-      {...animationStates.whileInView(true)}
+}) => (
+  <motion.div
+    className={styles.projectCard}
+    variants={animationConfigs.rotate(15, -15)}
+    transition={transitionConfigs.spring(1.25, 0, 0.2)}
+    {...animationStates.whileInView(true)}
+  >
+    <div
+      className={styles.projectCardHeader}
+      style={{ backgroundColor: color }}
     >
-      <div
-        className={styles.projectCardHeader}
-        style={{ backgroundColor: color }}
-      >
-        <motion.p
-          className={styles.projectCardText}
-          variants={animationConfigs.opacity(0, 0)}
-          transition={transitionConfigs.simple(0.2, 0.8)}
-          {...animationStates.whileInView(true)}
-        >
-          {name}
-        </motion.p>
-        <motion.p
-          className={styles.projectCardSubhead}
-          variants={animationConfigs.opacity(0, 0)}
-          transition={transitionConfigs.simple(0.2, 1)}
-          {...animationStates.whileInView(true)}
-        >
-          {subhead}
-        </motion.p>
-      </div>
-      <motion.div
-        className={styles.projectCardBody}
+      <motion.p
+        className={styles.projectCardText}
         variants={animationConfigs.opacity(0, 0)}
-        transition={transitionConfigs.simple(0.2, 1.4)}
+        transition={transitionConfigs.simple(0.2, 0.8)}
         {...animationStates.whileInView(true)}
       >
-        {description}
-      </motion.div>
+        {name}
+      </motion.p>
+      <motion.p
+        className={styles.projectCardSubhead}
+        variants={animationConfigs.opacity(0, 0)}
+        transition={transitionConfigs.simple(0.2, 1)}
+        {...animationStates.whileInView(true)}
+      >
+        {subhead}
+      </motion.p>
+    </div>
+    <motion.div
+      className={styles.projectCardBody}
+      variants={animationConfigs.opacity(0, 0)}
+      transition={transitionConfigs.simple(0.2, 1.4)}
+      {...animationStates.whileInView(true)}
+    >
+      {description}
     </motion.div>
-  );
-}
+  </motion.div>
+);
 
 ProjectCard.propTypes = {
   name: PropTypes.string.isRequired,
