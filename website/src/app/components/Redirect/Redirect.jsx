8e8c6682187fcaca0e378f18615c0cc86b0edcfd
Alan Chang <alan@alanchang.xyz>
2024-02-20 03:53:59 -0800

    [Feb 2024] Support mobile Redirects + change email/venmo links

--- website/src/app/components/Redirect/Redirect.jsx
+++ website/src/app/components/Redirect/Redirect.jsx
@@ -1,13 +1,19 @@
 import { useEffect } from "react";
+import { isMobile } from "react-device-detect";
 import PropTypes from "prop-types";
 
-const Redirect = ({ url }) => {
-  useEffect(() => window.location.replace(url), []);
+const Redirect = ({ url, mobileUrl }) => {
+  useEffect(() => window.location.replace(isMobile && mobileUrl ? mobileUrl : url), []);
   return null;
 };
 
 Redirect.propTypes = {
   url: PropTypes.string.isRequired,
+  mobileUrl: PropTypes.string,
+};
+
+Redirect.defaultProps = {
+  mobileUrl: null,
 };
 
 export default Redirect;
